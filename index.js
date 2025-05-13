23f4b669f14f1fa7feec7156e1538325ce684ea9
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-05-12 21:42:09 -0500

    fix: ensure server starts listening on the specified port

--- index.js
+++ index.js
@@ -197,5 +197,7 @@ app.get("/", (req, res) => {
   res.send("Track 2 Hired");
 });
 
-
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
 export default app;
